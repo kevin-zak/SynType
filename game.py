a0909797272c94c449b9b24a8d6fe87c3d341e57
kev011701 <kev011701@gmail.com>
2023-02-11 17:06:45 -0500

    Added basic typing to screen

--- game.py
+++ game.py
@@ -4,8 +4,10 @@ class Game():
     def __init__(self):
         pygame.init()
 
-        self.running, self.playing = True, False
-        self.LEFT_KEY, self.RIGHT_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+
+        
+
+        # Display Settings
         self.DISPLAY_W, self.DISPLAY_H = 1600, 900
         self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
         self.window = pygame.display.set_mode((self.DISPLAY_W, self.DISPLAY_H))
@@ -21,21 +23,34 @@ class Game():
         self.curr_menu = Menu(self)
 
 
-        # Set Font
-        #self.font = pygame.font.Font(os.path.join("assets", "ABCfont.ttf"), 16)
+
+        # Text Locations
+        self.input_startx = self.DISPLAY_W / 2.0 + 10
+        self.input_starty = 100
 
         #Some colors
         self.GREEN = (100, 255, 50)
         self.BLACK = (0,0,0)
         self.WHITE = (255,255,255)
 
+
+        #Game Status
+        self.running, self.playing = True, False
+        self.LEFT_KEY, self.RIGHT_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.input_text = ""
+
     def game_loop(self):
         while self.playing:
+            # Check for keypresses..
             self.check_events()
             
             self.display.fill(self.WHITE)
             self.draw_text("In Typing Game...", 20, 100, 100)
+
+            self.draw_text(self.input_text, 30, self.input_startx, self.input_starty)
             self.window.blit(self.display,(0,0))
+
+
             pygame.display.update()
             self.reset_keys()
 
@@ -48,14 +63,20 @@ class Game():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     self.START_KEY = True
-                if event.key == pygame.K_BACKSPACE:
+                    # If in progress of game... Submit!
+                    
+                elif event.key == pygame.K_BACKSPACE:
                     self.BACK_KEY = True
-                if event.key == pygame.K_RIGHT:
-                    self.RIGHT_KEY = True
-                if event.key == pygame.K_LEFT:
-                    self.LEFT_KEY = True
-                if event.key == pygame.K_ESCAPE:
+                    self.input_text = self.input_text[:-1]
+                
+                elif event.key == pygame.K_ESCAPE:
                     self.shutdown()
+
+                else: # Add keypress
+                    try:
+                        self.input_text += event.unicode
+                    except:
+                        pass
     
     def reset_keys(self):
         self.LEFT_KEY, self.RIGHT_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
