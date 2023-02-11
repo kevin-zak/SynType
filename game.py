f3432b6d87e42f96c135bc7eb282e2a29707c5de
kev011701 <kev011701@gmail.com>
2023-02-11 18:18:49 -0500

    formatted typing

--- game.py
+++ game.py
@@ -25,8 +25,8 @@ class Game():
 
 
         # Text Locations
-        self.input_startx = self.DISPLAY_W / 2.0 + 10
-        self.input_starty = 100
+        self.input_startx = self.DISPLAY_W / 2.0 
+        self.input_starty = 200
 
         #Some colors
         self.GREEN = (100, 255, 50)
@@ -37,7 +37,8 @@ class Game():
         #Game Status
         self.running, self.playing = True, False
         self.LEFT_KEY, self.RIGHT_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
-        self.input_text = ""
+        self.input_lines = []
+        self.curr_line = ""
 
     def game_loop(self):
         while self.playing:
@@ -45,18 +46,21 @@ class Game():
             self.check_events()
             
             self.display.fill(self.WHITE)
-            self.drawLeftPannel()
-            # self.draw_text("In Typing Game...", 20, 100, 100)
-
-            self.draw_text(self.input_text, 30, self.input_startx, self.input_starty)
+            self.drawPannels()
+            
             self.window.blit(self.display,(0,0))
 
 
             pygame.display.update()
             self.reset_keys()
 
-    def drawLeftPannel(self):
+
+
+
+
+    def drawPannels(self):
         y = 200
+        x = 0
         for line in self.lines:
             if line[0] == '\t':
                 x = 150
@@ -64,8 +68,24 @@ class Game():
                 x=100
             self.draw_code(line, 30, x, y)
             y+=50
+        y = 200
+        for line in self.input_lines:
+            if(len(line) == 0):
+                continue
+            if line[0] == '\t':
+                x = self.input_startx+150
+            else:
+                x = self.input_startx+100
+            self.draw_code(line, 30, x, y)
+            y+=50
+        if(len(self.curr_line) > 0 and self.curr_line[0] == '\t'):
+            x = self.input_startx+150
+        else:
+            x = self.input_startx+100
+        self.draw_code(self.curr_line, 30, x, y)
         
-        pygame.draw.line(self.window, self.BLACK, (400,0), (400,800), 2)
+
+    
 
     def check_events(self):
         for event in pygame.event.get():
@@ -74,18 +94,32 @@ class Game():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     self.START_KEY = True
-                    # If in progress of game... Submit!
+                    # If in progress of game... Move to new line
+                    self.input_lines += [self.curr_line]
+                    self.curr_line = ""
                     
                 elif event.key == pygame.K_BACKSPACE:
                     self.BACK_KEY = True
-                    self.input_text = self.input_text[:-1]
+                    #Check if at beginning of line...
+                    if len(self.curr_line) > 0:
+                        self.curr_line = self.curr_line[:-1]
+                    else:
+                        if(len(self.input_lines) > 0):
+                            self.curr_line = self.input_lines[-1]
+                            self.input_lines.pop()
+                        else:
+                            self.curr_line = ""
                 
                 elif event.key == pygame.K_ESCAPE:
                     self.shutdown()
 
+                elif event.key == pygame.K_TAB:
+                    # Has to add to current line of text...
+                    self.curr_line += "\t"
+
                 else: # Add keypress
                     try:
-                        self.input_text += event.unicode
+                        self.curr_line += event.unicode
                     except:
                         pass
     
